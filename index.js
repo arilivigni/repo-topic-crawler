0a0586d51cb07ab00af460872883483d3be4d081
Ari LiVigni <arilivigni@github.com>
2023-03-27 15:31:53 -0400

    update key/value for repository name

--- index.js
+++ index.js
@@ -55,7 +55,8 @@ async function main() {
                 path: repoFile
             })
             core.info(`Found ${repoFile} in repository ${repo.name} with topic ${repoTopic}}`)
-            collectedRepos.push("{repository: repo.name}", yaml.load(Buffer.from(response.content, 'base64').toString()));
+            const repository = {"repository":repo.name}
+            collectedRepos.push(repository, yaml.load(Buffer.from(response.content, 'base64').toString()));
         } catch (e) {
             core.debug(`Did NOT find ${repoFile} in repository ${repo.name} with topic ${repoTopic}}`)
             failed = true
